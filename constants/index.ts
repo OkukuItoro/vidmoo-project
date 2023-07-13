f84fa2fc75f6c5d6a1be9373377b8a5939fb6bc3
Okuku Itorobong <patrickokuku1@gmail.com>
2023-07-13 13:20:57 +0100

    constants: business logic

---
+++ constants/index.ts
@@ -0,0 +1,40 @@
+export const navLinks: string[] = [
+  "Home",
+  "Movies",
+  "Series",
+  "Blog",
+  "Contact",
+];
+
+export const upcomingMovies: object[] = [
+  {
+    title: "Women's World",
+    year: 2023,
+    duration: "2hr 30mins",
+    reviews: 4.3,
+  },
+  {
+    title: "Convent 3",
+    year: 2023,
+    duration: "1hr 30mins",
+    reviews: 4.7,
+  },
+  {
+    title: "Fatherhood",
+    year: 2023,
+    duration: "2hr 15mins",
+    reviews: 3.9,
+  },
+  {
+    title: "Cold Pursuit",
+    year: 2023,
+    duration: "1hr 45mins",
+    reviews: 4.6,
+  },
+];
+
+export const sideBarItems = [
+  { name: "explore", links: ["movies", "tv-shows"] },
+  { name: "advanced", links: ["insights", "genres"] },
+  { name: "for you", links: ["watch-later", "history"] },
+];
