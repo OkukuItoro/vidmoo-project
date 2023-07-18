da001327b6364f7fde245d0b14ac843065d21748
Okuku Itorobong <patrickokuku1@gmail.com>
2023-07-18 16:26:34 +0100

    test #24: cors conflict fix

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -1,12 +1,25 @@
 import { GenreProps, MoviesStateProps, ShowsStateProps } from "@/types";
 
+interface OptionsProps {
+  method: string;
+  headers: Headers | Record<string, string>;
+}
+
+const options: OptionsProps = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
+
 /* ACCESS API ROUTE TO FETCH DATA FROM THE DATABASE */
 export const fetchMovies = async (): Promise<MoviesStateProps[]> => {
-  const res = await fetch("http://localhost:3000/api/movies");
+  const res = await fetch("http://localhost:3000/api/movies", options);
   return res.json();
 };
 
 export const fetchShows = async (): Promise<ShowsStateProps[]> => {
-  const res = await fetch("http://localhost:3000/api/tv-shows");
+  const res = await fetch("http://localhost:3000/api/tv-shows", options);
   return res.json();
 };
